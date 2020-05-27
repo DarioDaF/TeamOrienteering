cd8246a49fadd8403dfa794203f593e70d27a30d
Dario Fagotto <dario.fagotto@gmail.com>
2020-05-27 17:51:30 +0200

    Multiple assigns

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 double ComputePointProfitPerDistance(idx_t car, idx_t dest, const TOP_Input &in, const TOP_Output& out) {
   if(out.Visited(dest)) return 0;
   int profit = in.Point(dest).Profit();
+  if(profit <= 0) return 0;
   double dist = out.SimulateMoveCar(car, dest).extraTravelTime;
   return profit / dist;
 }
@@ -19,24 +20,40 @@ void Solve(const TOP_Input &in, TOP_Output& out) {
   NumberRange<idx_t> carIdxs(in.Cars());
   NumberRange<idx_t> pointIdxs(in.Points());
   
-  //vector<bool> markedCars(in.Cars());
+  vector<bool> markedCars(in.Cars());
 
   while(true) {
     // Assign minimum travel time car
-    idx_t chosenCar = *min_element(carIdxs.begin(), carIdxs.end(), [&in, &out](idx_t c1, idx_t c2) {
+    idx_t chosenCar = *min_element(carIdxs.begin(), carIdxs.end(), [&in, &out, &markedCars](idx_t c1, idx_t c2) {
+      // If marked travel time is infinite!
+      if(markedCars[c1]) return false;
+      if(markedCars[c2]) return true;
       return out.TravelTime(c1) < out.TravelTime(c2);
     });
 
+    if(markedCars[chosenCar]) {
+      break; // All cars are marked
+    }
+
     // Search point with most bucks per distance
-    idx_t chosenPoint = *max_element(pointIdxs.begin(), pointIdxs.end(), [&in, &out, &chosenCar](idx_t p1, idx_t p2) {
-      return ComputePointProfitPerDistance(chosenCar, p1, in, out) < ComputePointProfitPerDistance(chosenCar, p2, in, out);
+    auto orderedPoints = pointIdxs.Vector();
+    sort(orderedPoints.begin(), orderedPoints.end(), [&in, &out, &chosenCar](auto p1, auto p2) {
+      return ComputePointProfitPerDistance(chosenCar, p1, in, out) > ComputePointProfitPerDistance(chosenCar, p2, in, out);
     });
 
-    cerr << "Moving car " << chosenCar << " to " << chosenPoint << endl;
+    idx_t i = 0;
+    bool done = false;
+    while(!done) {
+      if(i >= orderedPoints.size() || out.Visited(orderedPoints[i])) {
+        cerr << "Marking car " << chosenCar << endl;
+        markedCars[chosenCar] = true;
+        break;
+      }
 
-    if(!out.MoveCar(chosenCar, chosenPoint, false).feasible) {
-      //markedCars[chosenCar] = true;
-      break;
+      done = out.MoveCar(chosenCar, orderedPoints[i], false).feasible;
+      cerr << "Moving car " << chosenCar << " to " << orderedPoints[i] << (done ? " OK" : " FAILED") << endl;
+
+      ++i;
     }
   }
 
@@ -81,6 +98,10 @@ void Solve(const TOP_Input &in, TOP_Output& out) {
 */
 
 }
+
+
+#include <filesystem>
+namespace fs = std::filesystem;
 
 int main(int argc, char* argv[]) {
   if(argc != 2) {
